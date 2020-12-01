cc6570d082c5bb2c374e6d89faa0e4dfe1a89c8c
Rohitsingh9410 <75238367+Rohitsingh9410@users.noreply.github.com>
2020-12-01 09:30:16 +0530

    Add files via upload

---
+++ rohittask4.py
@@ -0,0 +1,9 @@
+a=input(int("enter average marks", 70<80))
+b=input(int("enter distinct marks", 80<90))
+c=input(int("enter elite marks)", 90<100)
+        if a<b:
+        print("average marks")
+        if ab<c:
+        print("distinct marks")
+        else:
+          print("elite marks")
