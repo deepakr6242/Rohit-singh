04e70e3e75d953ebd1a91010ccad9f14d7007792
deepakr6242 <deepakr6242@gmail.com>
2020-12-01 04:05:47 +0000

    Update rohittask4.py
    Review Done.
    Problem statement misunderstood.Please ensure to query before you start if you are not clear

--- rohittask4.py
+++ rohittask4.py
@@ -7,3 +7,25 @@ c=input(int("enter elite marks)", 90<100)
         print("distinct marks")
         else:
           print("elite marks")
+
+
+# Reviewed Code Below
+
+
+marks = int(input('Enter the marks: '))
+
+if 70 <= marks < 80:
+	    print('checking 70-75 category and 75-80 category....... ')
+	    if 70 <= marks <= 75:
+	        print('Average')
+	    else:
+	        print('above average')
+elif 80 <= marks <90:
+	    print('Distinction')
+elif 90 <= marks <=100:
+	    print('elite')
+elif 0 <= marks < 70:
+	    print('Poor students')
+else:
+		print('Give valid marks within 100')
+    
