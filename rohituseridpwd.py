6800d9bb6480ea1ed4d94418c1532edec84f8fad
Rohitsingh9410 <75238367+Rohitsingh9410@users.noreply.github.com>
2020-12-03 09:06:07 +0530

    Add files via upload

---
+++ rohituseridpwd.py
@@ -0,0 +1,10 @@
+for i in range(0,3):
+    userid=input("enter user id:")
+    password=input("enter password:")
+    if userid=="rohit" and password=="1234":
+        print("logged in")
+        break
+else:
+    print("incorrect id or password")
+
+
