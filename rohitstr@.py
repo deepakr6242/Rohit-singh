903660c047e86b49d134c41f56a7be4502142d7f
Rohitsingh9410 <75238367+Rohitsingh9410@users.noreply.github.com>
2020-12-09 09:01:41 +0530

    Add files via upload

---
+++ rohitstr@.py
@@ -0,0 +1,29 @@
+statement="Hello how are you@"
+if statement:
+    
+    x=statement.lower()
+    
+    vowels="a,e,i,o,u"
+
+    numbers="0,1,2,3,4,5,6,7,8,9"
+
+    symbols="@Αß©"
+
+    for sentence in x:
+
+        statement=x.find(sentence,x.index(sentence)+1,len(x))
+
+        if statement==-1:
+
+            if sentence in vowels:
+                print(f"unique character is {sentence}, it is vowel and its ord is {ord(sentence)}")
+            elif sentence in symbols:
+                print(f"{symbols.encode()} its a symbol")
+            elif sentence in numbers:
+                print("")
+            else:
+                print(f"unique character is {sentence}, it is consonent and its ord is {ord(sentence)}")
+
+else:
+    print("anything")
+                
